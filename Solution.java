4642b717229d45f07f40996ed6238779da4503fc
ekaterina.noni@rtlabs.ru <ekaterina.noni@rtlabs.ru>
2025-07-01 21:50:25 +0300

    Reading and converting strings

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        String str0 = console.nextLine();
+        String str1 = console.nextLine();
+        String str2 = console.nextLine();
+
+        System.out.println(str2);
+        System.out.println(str1.toUpperCase());
+        System.out.println(str0.toLowerCase());
+    }
+}
